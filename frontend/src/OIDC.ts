96ed307f1aa6a2cda7e43bbd68dd0b85365edb45
Julian Stitz <julian.stitz@gmail.com>
2023-11-05 15:40:08 +0100

    cleanup

--- frontend/src/OIDC.ts
+++ frontend/src/OIDC.ts
@@ -3,19 +3,11 @@ import {AuthProviderProps} from "react-oidc-context";
 
 export function oidcConfig(): AuthProviderProps {
     const redirectUri: URL = new URL(window.location.href);
-    const authority =  new URL(window.location.href);
-    authority.protocol = 'https';
-    authority.port = '8081';
-    authority.pathname = '/realms/shangri-la/';
-    authority.search = '';
-    authority.hash = '';
-
-    const config: AuthProviderProps = {
+    const authority: URL =  new URL(`https://${redirectUri.host}:8081/realms/shangri-la/`);
+    return {
         authority: authority.toString(),
         client_id: 'shangri-la',
         client_secret: 'FkWEXFhXt293Vh9xyx19AsnqGWNb3Rv8',
         redirect_uri: redirectUri.toString()
     };
-    console.log(config);
-    return config;
 }
